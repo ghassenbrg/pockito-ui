23559c92e4663867602651887208a305f0158a04
ghassenbrg <ghassen.brg16@gmail.com>
2025-10-19 02:51:12 +0200

    fix(wallets): ensure loading service is hidden on error and completion of wallet data loading

--- src/app/features/wallets/wallets.component.ts
+++ src/app/features/wallets/wallets.component.ts
@@ -50,9 +50,11 @@ export class WalletsComponent implements OnInit {
           if (b.orderPosition == null) return -1;
           return a.orderPosition - b.orderPosition;
         });
+        this.loadingService.hide();
       },
       error: () => {
         this.toastService.showError('loadingError');
+        this.loadingService.hide();
       },
       complete: () => {
         this.loadingService.hide();
